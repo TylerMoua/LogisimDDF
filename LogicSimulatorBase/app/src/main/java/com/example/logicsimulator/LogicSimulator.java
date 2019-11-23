d6f846041178de66cdf6e8fb7e3d40abfbd1e14d
TylerMoua <tylercmoua@gmail.com>
2019-11-23 15:35:32 -0800

    Fixed intro button-comment added

--- LogicSimulatorBase/app/src/main/java/com/example/logicsimulator/LogicSimulator.java
+++ LogicSimulatorBase/app/src/main/java/com/example/logicsimulator/LogicSimulator.java
@@ -93,6 +93,7 @@ public class LogicSimulator extends Activity {
 
                 //Re-Enables TouchScreen
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                //Disable introducing.
                 gridAndMenu.introducing=false;
             }
         });
